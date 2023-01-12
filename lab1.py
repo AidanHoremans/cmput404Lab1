eb76a144fed84bfa99ea120c34f10d81ed1853e4
Aidan Horemans <aidanjhoremans@gmail.com>
2023-01-11 22:34:49 -0700

    feat: script now prints out raw code and downloads a copy of itself

--- lab1.py
+++ lab1.py
@@ -3,3 +3,8 @@ import requests
 print(requests.__version__)
 response = requests.get("http://google.com")
 print(response.text)
+
+response = requests.get("https://raw.githubusercontent.com/AidanHoremans/cmput404Lab1/main/lab1.py")
+print(response.text)
+
+open("(1)lab1.py", 'w').write(response.text)
